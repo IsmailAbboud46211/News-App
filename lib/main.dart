0bf40b9b2fc628f9571362946e965fc6c60d94eb
IsmailAbboud46211 <ismailismail46211@gmail.com>
2024-04-30 11:21:21 +0300

    portraitUp ui

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MultiBlocProvider(
       providers: [
         BlocProvider(
